f0de4d6584cb0254117d79207f283968aeb6cf44
Amit palai <75921456+amkpa@users.noreply.github.com>
2022-04-28 12:31:04 +0530

    Add files via upload

---
+++ Programs/22.04.22/Array/src/com/company/arrays.java
@@ -0,0 +1,27 @@
+package com.company;
+
+public class arrays {
+    public static void main(String[] args){
+        int [] marks = new int[5];
+        int [] sheet = new int[2];
+
+
+
+        marks  [0] = 50;
+        marks  [1] = 80;
+        marks  [2] = 78;
+        marks  [3] = 50;
+        marks  [4] = 88;
+
+        sheet[0]=7;
+        sheet[1]=8;
+
+        System.out.println(marks[2]);
+        System.out.println(marks[1]);
+
+
+
+
+    }
+}
+
